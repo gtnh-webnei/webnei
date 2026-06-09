ef11f6b4abac08ed5b17ce06176f669908eea6e1
XSana <nico@niconi.cn>
2026-06-09 15:49:54 +0800

    Remove unused mod option lookups

--- src/main/java/moe/takochan/webnei/item/ItemModOptionRepository.java
+++ src/main/java/moe/takochan/webnei/item/ItemModOptionRepository.java
@@ -1,7 +1,6 @@
 package moe.takochan.webnei.item;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,6 +10,4 @@ public interface ItemModOptionRepository
         extends JpaRepository<ItemModOptionEntity, ItemModOptionEntity.ModOptionId> {
 
     List<ItemModOptionEntity> findByDatasetIdOrderByNameAscModIdAsc(String datasetId);
-
-    Optional<ItemModOptionEntity> findByDatasetIdAndModId(String datasetId, String modId);
 }
