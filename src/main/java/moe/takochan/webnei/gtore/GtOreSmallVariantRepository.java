ce4b206bb5277f4976bb80536265719f984b914a
XSana <nico@niconi.cn>
2026-06-09 16:17:47 +0800

    Minimize small ore variant repository

--- src/main/java/moe/takochan/webnei/gtore/GtOreSmallVariantRepository.java
+++ src/main/java/moe/takochan/webnei/gtore/GtOreSmallVariantRepository.java
@@ -1,14 +1,9 @@
 package moe.takochan.webnei.gtore;
 
-import java.util.List;
-
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GtOreSmallVariantRepository
         extends JpaRepository<GtOreSmallVariantEntity, GtOreSmallVariantEntity.OreSmallVariantId> {
-
-    List<GtOreSmallVariantEntity> findByDatasetIdAndOreGenName(String datasetId, String oreGenName, Sort sort);
 }
